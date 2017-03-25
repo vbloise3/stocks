7b8d1407098258397e7a64ebcb503c620a989b59
Vince Bloise <vbloise3@verizon.net>
2017-03-25 15:28:44 -0400

    Added VWO to default list of stocks

--- src/app/services/stocks.service.ts
+++ src/app/services/stocks.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
-let stocks: Array<string> = ['AAPL', 'GOOG', 'FB', 'AMZN', 'TWTR'];
+let stocks: Array<string> = ['AAPL', 'GOOG', 'FB', 'AMZN', 'TWTR', 'VWO'];
 let service: string = 'https://angular2-in-action-api.herokuapp.com';
 
 export interface StockInterface {
