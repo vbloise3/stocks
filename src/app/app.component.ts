151797ab646679ac9d554fa1ba4bc9bb67305508
Vince Bloise <vbloise3@verizon.net>
2017-04-01 14:25:48 -0400

    Cleaned up app.component to remove unnecessary loading of stocks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,8 @@
 import { Component } from '@angular/core';
-import { StocksService, StockInterface } from './services/stocks.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  stocks: Array<StockInterface>;
-
-  constructor(service: StocksService) {
-    service.load(['APPL']).subscribe(stocks => {
-      this.stocks = stocks.json();
-    });
-  }
-}
+export class AppComponent {}
